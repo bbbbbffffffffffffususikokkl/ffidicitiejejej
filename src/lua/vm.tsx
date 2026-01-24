e8df6f76874958039cf12c6c16bc7d339ba589ae
polnoref <yupiooipyupio@gmail.com>
2026-01-24 17:35:02 +0500

    Add files via upload

--- src/lua/vm.tsx
+++ src/lua/vm.tsx
@@ -14,20 +14,20 @@ export function generateVM(bytecode: any): string {
         local ops = {
             [0] = function(i) Stk[i[2]] = Stk[i[3]] end,
             [1] = function(i) Stk[i[2]] = Const[i[3]+1] end,
-            [2] = function(i) Stk[i[2]] = Env[Const[i[3]+1]] end,
+            [2] = function(i) Stk[i[2]] = Env[Const[i[3]+1]] or _G[Const[i[3]+1]] end,
             [3] = function(i) Env[Const[i[3]+1]] = Stk[i[2]] end,
             [4] = function(i) Stk[i[2]] = Stk[i[3]][Stk[i[4]]] end,
             [5] = function(i) Stk[i[2]][Stk[i[3]]] = Stk[i[4]] end,
             [6] = function(i) 
                 local A, B, C = i[2], i[3], i[4]
+                local func = Stk[A]
+                if type(func) ~= "function" then return end
                 local args = {}
-                if B > 1 then for idx=1, B-1 do args[idx] = Stk[A+idx] end
-                elseif B == 0 then for idx=A+1, #Stk do args[#args+1] = Stk[idx] end end
-                local results = {pcall(Stk[A], unpack(args))}
-                if results[1] then
-                    if C > 1 then for idx=0, C-2 do Stk[A+idx] = results[idx+2] end
-                    elseif C == 0 then for idx=2, #results do Stk[A+idx-2] = results[idx] end end
-                else error(results[2]) end
+                if B > 1 then for idx = 1, B - 1 do args[idx] = Stk[A + idx] end end
+                local results = {pcall(func, unpack(args))}
+                if results[1] and C > 1 then
+                    for idx = 0, C - 2 do Stk[A + idx] = results[idx + 2] end
+                end
             end,
             [7] = function(i) pc = #Inst + 1 end,
             [8] = function(i) Stk[i[2]] = Stk[i[3]] + Stk[i[4]] end,
@@ -40,8 +40,6 @@ export function generateVM(bytecode: any): string {
             [17] = function(i) Stk[i[2]] = Stk[i[3]] .. Stk[i[4]] end,
             [18] = function(i) pc = pc + i[2] end,
             [19] = function(i) if Stk[i[3]] ~= Stk[i[4]] then pc = pc + 1 end end,
-            [20] = function(i) if Stk[i[3]] == Stk[i[4]] then pc = pc + 1 end end,
-            [21] = function(i) if Stk[i[3]] <= Stk[i[4]] then pc = pc + 1 end end,
             [22] = function(i) Stk[i[2]] = {} end,
         }
         while pc <= #Inst do
