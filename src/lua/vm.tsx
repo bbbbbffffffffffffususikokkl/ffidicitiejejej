9bf584ea836f601f02a76c9e9d1f1cfdbc7586f3
polnoref <yupiooipyupio@gmail.com>
2026-01-24 17:28:48 +0500

    Add files via upload

--- src/lua/vm.tsx
+++ src/lua/vm.tsx
@@ -18,33 +18,16 @@ export function generateVM(bytecode: any): string {
             [3] = function(i) Env[Const[i[3]+1]] = Stk[i[2]] end,
             [4] = function(i) Stk[i[2]] = Stk[i[3]][Stk[i[4]]] end,
             [5] = function(i) Stk[i[2]][Stk[i[3]]] = Stk[i[4]] end,
-                        [6] = function(i) 
-                local A = i[2]
-                local func = Stk[A]
-                
-                if not func then 
-                    warn("Vexile VM Fatal: Attempted to call nil at PC="..pc)
-                    return
-                end
-                
-                local B = i[3]
-                local results = {_pcall(func, __unpack(Stk, A + 1, B))}
-                
+            [6] = function(i) 
+                local A, B, C = i[2], i[3], i[4]
+                local args = {}
+                if B > 1 then for idx=1, B-1 do args[idx] = Stk[A+idx] end
+                elseif B == 0 then for idx=A+1, #Stk do args[#args+1] = Stk[idx] end end
+                local results = {pcall(Stk[A], unpack(args))}
                 if results[1] then
-                    local C = i[4]
-                    if C == 1 then
-                    elseif C == 2 then
-                        Stk[A] = results[2]
-                    else
-                        local edx = 0
-                        for idx = A, C do
-                            edx = edx + 1
-                            Stk[idx] = results[edx + 1]
-                        end
-                    end
-                else
-                    error("Vexile Error: " .. tostring(results[2]), 0)
-                end
+                    if C > 1 then for idx=0, C-2 do Stk[A+idx] = results[idx+2] end
+                    elseif C == 0 then for idx=2, #results do Stk[A+idx-2] = results[idx] end end
+                else error(results[2]) end
             end,
             [7] = function(i) pc = #Inst + 1 end,
             [8] = function(i) Stk[i[2]] = Stk[i[3]] + Stk[i[4]] end,
@@ -57,11 +40,12 @@ export function generateVM(bytecode: any): string {
             [17] = function(i) Stk[i[2]] = Stk[i[3]] .. Stk[i[4]] end,
             [18] = function(i) pc = pc + i[2] end,
             [19] = function(i) if Stk[i[3]] ~= Stk[i[4]] then pc = pc + 1 end end,
+            [20] = function(i) if Stk[i[3]] == Stk[i[4]] then pc = pc + 1 end end,
+            [21] = function(i) if Stk[i[3]] <= Stk[i[4]] then pc = pc + 1 end end,
             [22] = function(i) Stk[i[2]] = {} end,
         }
         while pc <= #Inst do
-            local i = Inst[pc]
-            pc = pc + 1
+            local i = Inst[pc]; pc = pc + 1
             local f = ops[i[1]]
             if f then f(i) end
         end
