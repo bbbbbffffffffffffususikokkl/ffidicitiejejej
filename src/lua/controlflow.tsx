fc9d16687e8789468843c520f7306cc161695add
polnoref <yupiooipyupio@gmail.com>
2026-02-01 12:18:09 +0500

    Add files via upload

--- src/lua/controlflow.tsx
+++ src/lua/controlflow.tsx
@@ -1,7 +1,3 @@
-// Control Flow Flattering
-// By Vexile
-import { genVar, obfNum } from './antitamper';
-
 export function getDeadCode(preset: string): string {
     if (preset === "Test") return "";
     
@@ -10,13 +6,22 @@ export function getDeadCode(preset: string): string {
     if (preset === "High" || preset === "Custom") count = 2000;
 
     const junkTable = genVar(8);
-    let junk = `local ${junkTable} = {}; `;
+    let junk = `local ${junkTable} = {};\n`;
+    
+    for (let i = 0; i < Math.min(100, count); i++) {
+        const k = Math.floor(Math.random() * 500);
+        junk += `${junkTable}[${obfNum(k)}] = ${obfNum(0)};\n`;
+    }
     
     for (let i = 0; i < count; i++) {
         const k = Math.floor(Math.random() * 500);
         const v = Math.floor(Math.random() * 1000);
-
-        junk += `${junkTable}[${obfNum(k)}] = (${junkTable}[${obfNum(k)}] or ${obfNum(0)}) + ${obfNum(v)}; `;
+        
+        if (Math.random() > 0.5) {
+            junk += `${junkTable}[${obfNum(k)}] = ${obfNum(v)}; `;
+        } else {
+            junk += `if ${junkTable}[${obfNum(k)}] then ${junkTable}[${obfNum(k)}] = ${junkTable}[${obfNum(k)}] + ${obfNum(v)} else ${junkTable}[${obfNum(k)}] = ${obfNum(v)} end; `;
+        }
         
         if (i % 20 === 0) junk += "\n";
     }
